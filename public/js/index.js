06a9118657de0d56a5cf599bdc742c757ffbfe2c
Charles Holbrow <charlesholbrow@gmail.com>
2017-01-27 12:34:15 -0500

    comment index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
-// Create the renderer
+// Create the renderer. There's a great explaination of
+// resolution parameters here:
+// http://www.goodboydigital.com/pixi-js-v2-fastest-2d-webgl-renderer/
 var renderer = PIXI.autoDetectRenderer(256, 256);
+
 // I believe this tells the pixi engine when we resize the dom
 // object, but I'm not totally sure.
 renderer.autoResize = true;
-// Create a container object called the `stage`
-var stage = new PIXI.Container();
-// Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
 
+// Create a container object called the `stage`, and render it
+var stage = new PIXI.Container();
+renderer.render(stage);
 
 window.onload = function() {
   //Add the canvas to the HTML document
