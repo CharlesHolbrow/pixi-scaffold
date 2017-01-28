ec714a32fbf535eb71f990476c6b33fb833c8c44
Charles Holbrow <charlesholbrow@gmail.com>
2017-01-28 16:19:15 -0500

    more general, more useful

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,52 @@
-// Create the renderer. There's a great explaination of
-// resolution parameters here:
-// http://www.goodboydigital.com/pixi-js-v2-fastest-2d-webgl-renderer/
-var renderer = PIXI.autoDetectRenderer(256, 256);
+//Aliases
+var Container = PIXI.Container,
+    loader    = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite    = PIXI.Sprite,
+    Rectangle = PIXI.Rectangle;
 
-// I believe this tells the pixi engine when we resize the dom
-// object, but I'm not totally sure.
-renderer.autoResize = true;
+var renderer
+  , stage;
 
-// Create a container object called the `stage`, and render it
-var stage = new PIXI.Container();
-renderer.render(stage);
+
+function updateRendererSize(){
+  var width = renderer.view.scrollWidth;
+  var height = renderer.view.scrollHeight;
+  if (!renderer) return;
+  renderer.resize(width, height);
+}
+
+function setup() {
+
+  // oryx: 16x24
+  // mine: 28x35 (tile), 30x37 (cell)
+  var texture = loader.resources["/img/elements9x3.png"].texture;
+  texture.frame = new Rectangle(30, 37, 30, 37);
+  var terrain = new PIXI.extras.TilingSprite(texture);
+
+  stage.addChild(terrain);
+  renderer.render(stage);
+}
+
 
 window.onload = function() {
-  //Add the canvas to the HTML document
+
+  // Create the renderer.
+  renderer = PIXI.autoDetectRenderer(512, 512);
+  renderer.roundPixels = true;
+
+  // Add the canvas to the HTML document. This will cause the
+  // size of the canvas to update as per the canvas css
+  // settings. After the canvas has been given the correct size
+  // we update the RendererSize
   document.getElementById("main-view").appendChild(renderer.view);
+  updateRendererSize()
+
+  stage = new PIXI.Container();
+
+  loader
+    .add("/img/elements9x3.png")
+    .load(setup)
 };
 
-window.addEventListener("resize", function(event){ 
-  // example of re-sizing AND re-scaling
-  // https://github.com/kittykatattack/scaleToWindow/blob/master/scaleToWindow.js
-  // console.log('resize', window.innerWidth, window.innerHeight);
-});
-
+window.addEventListener("resize", updateRendererSize);
